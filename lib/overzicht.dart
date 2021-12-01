cc081217e05772d4f762332242e7a8489b1a61fa
Ferry Utomo <huferry@outlook.com>
2021-12-01 22:46:17 +0100

    Fill info and add user name

--- lib/overzicht.dart
+++ lib/overzicht.dart
@@ -4,8 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Overzicht extends StatelessWidget {
+  final incidentInfo = IncidentInfo();
+
   void onMarkerTap(Map<String, dynamic> incident) {
-    print(incident['title']);
+    incidentInfo.onUpdate(incident);
   }
 
   @override
@@ -34,8 +36,11 @@ class Overzicht extends StatelessWidget {
           return document.data()! as Map<String, dynamic>;
         }).toList();
 
-        return Stack(
-            children: [buildBase(context, incidents), buildUser(context)]);
+        return Stack(children: [
+          buildBase(context, incidents),
+          buildUser(context),
+          buildUserName(context)
+        ]);
       },
     );
   }
@@ -46,22 +51,42 @@ class Overzicht extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Expanded(child: IncidentMap(incidents, onMarkerTap)),
-        IncidentInfo()
+        incidentInfo
       ],
     );
   }
 
   Widget buildUser(BuildContext context) {
     return Positioned(
-        right: 12,
-        top: 13,
+        right: 30,
+        top: 20,
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(43),
+          borderRadius: BorderRadius.circular(30),
           child: Image.network(
               'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kaley-cuoco-1615625941.jpg',
               fit: BoxFit.cover,
-              height: 86,
-              width: 86),
+              height: 60,
+              width: 60),
+        ));
+  }
+
+  Widget buildUserName(BuildContext context) {
+    return Positioned(
+        right: 94,
+        top: 16,
+        child: Text(
+          'Kaley (MKS BO)',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+            shadows: <Shadow>[
+              Shadow(
+                offset: Offset(1.0, 1.0),
+                blurRadius: 8.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              )
+            ],
+          ),
         ));
   }
 }
@@ -91,9 +116,11 @@ class MyClipper extends CustomClipper<Path> {
     path.moveTo(15, 15);
     path.lineTo(15, size.height - 15);
     path.lineTo(size.width - 15, size.height - 15);
-    path.lineTo(size.width - 15, 95);
-    path.arcToPoint(Offset(size.width - 95, 15),
+    path.lineTo(size.width - 15, 60);
+    path.arcToPoint(Offset(size.width - 102, 35),
         radius: Radius.circular(10), largeArc: true);
+    path.lineTo(size.width - 202, 35);
+    path.lineTo(size.width - 202, 15);
     path.close();
     return path;
   }
@@ -144,15 +171,110 @@ class MapDisplayState extends State<MapDisplay> {
   }
 }
 
-class IncidentInfo extends StatelessWidget {
+class IncidentInfo extends StatefulWidget {
+  final _state = IncidentInfoState();
+
+  void onUpdate(Map<String, dynamic> newIncient) {
+    _state.onUpdate(newIncient);
+  }
+
+  @override
+  State<StatefulWidget> createState() => _state;
+}
+
+class IncidentInfoState extends State<IncidentInfo> {
+  Map<String, dynamic> incident = Map<String, dynamic>();
+
+  void onUpdate(Map<String, dynamic> newIncident) {
+    setState(() {
+      incident = newIncident;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.only(bottom: 15, left: 15, right: 15),
+        padding: EdgeInsets.all(15),
         decoration: BoxDecoration(
             color: Color(0xFF737373),
             borderRadius: BorderRadius.circular(10.0)),
         height: 200,
-        child: Text('hello'));
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [getIncidentId(), getTitle(), getDetail()]));
   }
+
+  Widget getTitle() => Text(
+        incident['title'] ?? '(incident)',
+        style: TextStyle(
+            color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+      );
+
+  Widget getIncidentId() => Text(
+        incident['incidentId']?.toString() ?? '(id)',
+        style: TextStyle(color: Colors.white, fontSize: 12),
+      );
+
+  Widget getDetail() => Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: getLeftColumn()),
+            VerticalDivider(width: 1.0),
+            Expanded(child: getRighColumn())
+          ],
+        ),
+      );
+
+  Widget getLeftColumn() => Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            getField('melder', incident['melder']),
+            getField(
+                'treindienstleidergebied', incident['treindienstleidergebied']),
+            getField('infraclaim', incident['infraclaim'])
+          ],
+        ),
+      );
+
+  Widget getRighColumn() => Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            getField('tis', incident['tis']),
+            getField('slachtoffers', incident['slachtofers']),
+            getField('impact', incident['impact'])
+          ],
+        ),
+      );
+
+  final fieldStyle =
+      TextStyle(color: Colors.white, fontSize: 10, fontWeight: FontWeight.bold);
+
+  final valueStyle = TextStyle(color: Color(0xFFCECECE), fontSize: 12);
+
+  Widget getField(String name, String? value) => Container(
+        margin: EdgeInsets.only(top: 12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              name,
+              style: fieldStyle,
+            ),
+            Text(
+              value ?? '',
+              style: valueStyle,
+            )
+          ],
+        ),
+      );
 }
